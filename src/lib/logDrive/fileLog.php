3fa42b406d6af01ece64d61cf66fbeec6c8a9a4f
mac <mac@macdeMacBook-Pro.local>
2017-11-29 20:21:42 +0800

    update

--- src/lib/logDrive/fileLog.php
+++ src/lib/logDrive/fileLog.php
@@ -12,7 +12,7 @@ class fileLog extends BaseLog
 		$path = self::$config['log']['path'];
 		if(!is_dir($path.self::$logtime->format('YmdH')))
 		{
-			mkdir($path.self::$logtime->format('YmdH'), '0777', true);
+			mkdir($path.self::$logtime->format('YmdH'), 0777, true);
 		}
 		return file_put_contents($path.self::$logtime->format('YmdH').'/'.$file.'.log', self::$logtime->format('Y-m-d H:i:s').$message.PHP_EOL, FILE_APPEND);
 	}
